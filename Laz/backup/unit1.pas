3bd8fe47e3c4c467270b24c6f83b5900dbb03dd5
kopotilo <62011819+kopotilo@users.noreply.github.com>
2020-03-24 13:50:24 +0300

    Add files via upload

--- Laz/backup/unit1.pas
+++ Laz/backup/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls, Grids;
 
 type
 
@@ -29,6 +29,9 @@ type
     MenuItem2: TMenuItem;
     MenuItem20: TMenuItem;
     MenuItem21: TMenuItem;
+    MenuItem22: TMenuItem;
+    MenuItem23: TMenuItem;
+    MenuItem24: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
@@ -38,6 +41,7 @@ type
     MenuItem9: TMenuItem;
     OpenDialog1: TOpenDialog;
     SaveDialog1: TSaveDialog;
+    StringGrid1: TStringGrid;
     procedure FormCreate(Sender: TObject);
     procedure Memo1Change(Sender: TObject);
     procedure MenuItem10Click(Sender: TObject);
@@ -46,6 +50,12 @@ type
     procedure MenuItem13Click(Sender: TObject);
     procedure MenuItem15Click(Sender: TObject);
     procedure MenuItem16Click(Sender: TObject);
+    procedure MenuItem18Click(Sender: TObject);
+    procedure MenuItem19Click(Sender: TObject);
+    procedure MenuItem21Click(Sender: TObject);
+    procedure MenuItem22Click(Sender: TObject);
+    procedure MenuItem23Click(Sender: TObject);
+    procedure MenuItem24Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
     procedure MenuItem5Click(Sender: TObject);
     procedure MenuItem6Click(Sender: TObject);
@@ -55,6 +65,7 @@ type
   private
 
   public
+
 
   end;
 
@@ -66,6 +77,7 @@ implementation
 
 {$R *.lfm}
 
+
 Procedure SaveAs;
 begin
   if Form1.SaveDialog1.Execute then
@@ -74,6 +86,16 @@ begin
     FileWork:=Form1.SaveDialog1.FileName;
   end;
 end;
+procedure TForm1.Memo1Change(Sender: TObject);
+var i: integer;
+begin
+  StringGrid1.RowCount := Memo1.Lines.Count;
+  StringGrid1.DefaultRowHeight := Memo1.Font.GetTextHeight(' ')-1;
+  for i := 0 to StringGrid1.RowCount-1
+  do StringGrid1.Cells[0,i] := IntToStr(i+1);
+end;
+
+
 
 { TForm1 }
 
@@ -103,10 +125,7 @@ begin
   Memo1.SelectAll;
 end;
 
-procedure TForm1.Memo1Change(Sender: TObject);
-begin
 
-end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
@@ -128,11 +147,41 @@ begin
 
 end;
 
+procedure TForm1.MenuItem18Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem19Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem21Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem22Click(Sender: TObject);
+begin
+  Memo1.color:=clblack;
+end;
+
+procedure TForm1.MenuItem23Click(Sender: TObject);
+begin
+  Memo1.color:=clwhite;
+end;
+
+procedure TForm1.MenuItem24Click(Sender: TObject);
+begin
+  Memo1.color:=clred;
+end;
+
 procedure TForm1.MenuItem5Click(Sender: TObject);
 begin
-  FileWork:='';
-  Memo1.Clear;
+
 end;
+
 
 procedure TForm1.MenuItem6Click(Sender: TObject);
 begin
@@ -153,6 +202,9 @@ procedure TForm1.MenuItem9Click(Sender: TObject);
 begin
    Memo1.CutToClipboard;
 end;
+
+
+
 
 end.
 
